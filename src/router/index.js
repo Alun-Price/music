78f9d48e4aadda970a51a6baa7df6d1dd16e00c2
Alun Price <alunprice089@gmail.com>
2023-02-07 21:54:34 +0000

    s12 149 Added tailwind styles for active links, named routes and set up catchAll and redirect routes

--- src/router/index.js
+++ src/router/index.js
@@ -5,22 +5,34 @@ import Manage from "@/views/Manage.vue";
 
 const routes = [
   {
+    name: "home",
     path: "/",
     component: Home,
   },
   {
+    name: "about",
     path: "/about",
     component: About,
   },
   {
-    path: "/manage",
+    name: "manage",
+    path: "/manage-music",
     component: Manage,
+  },
+  {
+    path: "/manage",
+    redirect: "/manage-music",
+  },
+  {
+    path: "/:catchAll(.*)*",
+    redirect: { name: "home" },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+  linkExactActiveClass: "text-yellow-500",
 });
 
 export default router;
